7a9a3273de2a21aec40e52e0ee0e02fd3890107b
m-tanner <tanner.mbt@gmail.com>
2020-04-27 09:38:25 -0700

    pw change, email change, pw reset, user roles, tests pass

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -3,7 +3,7 @@ import time
 import pytest
 
 from src.app import create_app, db
-from src.app.models import User
+from src.app.models import User, Role, Permission, AnonymousUser
 
 
 @pytest.fixture
@@ -12,6 +12,7 @@ def resource():
     app_context = app.app_context()
     app_context.push()
     db.create_all()
+    Role.insert_roles()
     yield "resource"
     db.session.remove()
     db.drop_all()
@@ -66,3 +67,90 @@ def test_expired_confirmation_token(resource):
     token = user.generate_confirmation_token(expiration=1)  # in seconds
     time.sleep(2)  # in seconds
     assert not user.confirm(token)
+
+
+def test_valid_reset_token(resource):
+    u = User(password='cat')
+    db.session.add(u)
+    db.session.commit()
+    token = u.generate_reset_token()
+    assert User.reset_password(token, 'dog')
+    assert u.verify_password('dog')
+
+
+def test_invalid_reset_token(resource):
+    u = User(password='cat')
+    db.session.add(u)
+    db.session.commit()
+    token = u.generate_reset_token()
+    assert not User.reset_password(token + 'a', 'horse')
+    assert u.verify_password('cat')
+
+
+def test_valid_email_change_token(resource):
+    u = User(email='john@example.com', password='cat')
+    db.session.add(u)
+    db.session.commit()
+    token = u.generate_email_change_token('susan@example.org')
+    assert u.change_email(token)
+    assert u.email == 'susan@example.org'
+
+
+def test_invalid_email_change_token(resource):
+    u1 = User(email='john@example.com', password='cat')
+    u2 = User(email='susan@example.org', password='dog')
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+    token = u1.generate_email_change_token('david@example.net')
+    assert not u2.change_email(token)
+    assert u2.email == 'susan@example.org'
+
+
+def test_duplicate_email_change_token(resource):
+    u1 = User(email='john@example.com', password='cat')
+    u2 = User(email='susan@example.org', password='dog')
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+    token = u2.generate_email_change_token('john@example.com')
+    assert not u2.change_email(token)
+    assert u2.email == 'susan@example.org'
+
+
+def test_user_role(resource):
+    u = User(email='john@example.com', password='cat')
+    assert u.can(Permission.FOLLOW)
+    assert u.can(Permission.COMMENT)
+    assert u.can(Permission.WRITE)
+    assert not u.can(Permission.MODERATE)
+    assert not u.can(Permission.ADMIN)
+
+
+def test_moderator_role(resource):
+    r = Role.query.filter_by(name='Moderator').first()
+    u = User(email='john@example.com', password='cat', role=r)
+    assert u.can(Permission.FOLLOW)
+    assert u.can(Permission.COMMENT)
+    assert u.can(Permission.WRITE)
+    assert u.can(Permission.MODERATE)
+    assert not u.can(Permission.ADMIN)
+
+
+def test_administrator_role(resource):
+    r = Role.query.filter_by(name='Administrator').first()
+    u = User(email='john@example.com', password='cat', role=r)
+    assert u.can(Permission.FOLLOW)
+    assert u.can(Permission.COMMENT)
+    assert u.can(Permission.WRITE)
+    assert u.can(Permission.MODERATE)
+    assert u.can(Permission.ADMIN)
+
+
+def test_anonymous_user(resource):
+    u = AnonymousUser()
+    assert not u.can(Permission.FOLLOW)
+    assert not u.can(Permission.COMMENT)
+    assert not u.can(Permission.WRITE)
+    assert not u.can(Permission.MODERATE)
+    assert not u.can(Permission.ADMIN)
